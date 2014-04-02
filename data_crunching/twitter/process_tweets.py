8aaa097961dc82dff5d627f796dec63fddaff771
Stefan Mihaila <stefan.mihaila@epfl.ch>
2014-04-02 17:24:41 +0200

    Tweets processor changes

--- data_crunching/twitter/process_tweets.py
+++ data_crunching/twitter/process_tweets.py
@@ -9,7 +9,7 @@ from to_csv import get_column, get_tweets
 
 
 def usage():
-    print '''python %s <filter> [input file] [output file]'
+    print '''python %s <filter1,filter2,..,filterN> [input file] [output file]'
     Defaults: input file = ../data_collection/twitter/tweets.pickle
               output file = filter_results/tweets-<filtername>.pickle
     To learn how to create a filter, see filters/example.py''' % sys.argv[0]
@@ -19,18 +19,20 @@ def main():
     if len(sys.argv) < 2:
         usage()
         sys.exit(-1)
-    filter_ = sys.argv[1]
+    filters = sys.argv[1].split(',')
     input_file = sys.argv[2] if len(sys.argv) > 2 else \
             '../../data_collection/twitter/tweets.pickle'
     output_file = sys.argv[3] if len(sys.argv) > 3 else \
-            ('filter_results/tweets-%s.pickle' % filter_)
+            ('filter_results/tweets-%s.pickle' % '-'.join(filters))
     tweets = get_tweets(input_file)
 
-    mod = __import__('filters.' + filter_, fromlist=['*'])
+
+    for f in filters:
+         tweets = __import__('filters.' + f,
+                 fromlist=['*']).process_tweets(tweets)
 
     with open(output_file, 'w+b') as f:
-        pickle.dump([row for row in mod.process_tweets(tweets)], f,
-                protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(list(tweets), f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
