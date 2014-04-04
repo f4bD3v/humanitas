9b93243bc0c44be154d8e87e4d52aaad001790b1
Anton Ovchinnikov <revolver112@gmail.com>
2014-04-04 16:37:45 +0400

    parse_weekly.py: output in tabular format

--- data_crunching/india_timeseries/parse_weekly.py
+++ data_crunching/india_timeseries/parse_weekly.py
@@ -3,6 +3,7 @@
 import re
 import sys
 import india_data
+import os
 
 def usage():
     print """
@@ -15,22 +16,41 @@ Example:
     python2 parse_weekly.py 02_03_2012.csv
     """
 
+# 02/01/2000,week,India,Banda Aceh,milk,condensed,4000.0
+
+date_string = ''
+cur_product = ''
 def print_line(line):
     # Product name
-    if re.search(r'###', line):
-        print line
+    #if re.search(r'###', line):
+    #    print line
+
+    # Parse product
+    match = re.search(r'(\w+)\s*(?:\((.+)\))?', cur_product)
+    if not match:
+        return
+    product_name = match.group(1)
+    subproduct_name = match.group(2)
+    if not subproduct_name:
+        subproduct_name = ''
 
     # Extract cities and prices for the current week
     for match in re.finditer(r'"([^"\d]+)","([\d\.]+)[^\d"]*","[^"]*","[^"]*"', line):
         city = match.group(1)
         price = match.group(2)
-        print "\"%s\": %s" % (city, price)
+        # print "\"%s\": %s" % (city, price)
+        print "%s,week,India,%s,%s,%s,%s" % \
+                (date_string, city, product_name, subproduct_name, price)
 
 def main(filename):
-    f   = open(filename,'r')
+    global date_string
+    global cur_product
+    date_string = re.sub('_', '/', os.path.basename(filename))
+    date_string = re.sub('\.\w+$', '', date_string)
+    f = open(filename,'r')
     prevline = ''
     all_products = india_data.all_products
-    
+    cur_product = ''
     for line in f:
         line = line.rstrip()
         if not re.search(r'"[\w\s]+"', line):
@@ -40,14 +60,13 @@ def main(filename):
             print_line(prevline)
             prevline = line
             continue
-        product = all_products.pop(0)
         if re.search(r'^,"[^"\d]+","', prevline):
             # Previous line belongs to new product
-            print_line('##### "' + product + '" #####')
+            cur_product = all_products.pop(0)
             print_line(prevline)
         else:
             print_line(prevline)
-            print_line('##### "' + product + '" #####')
+            cur_product = all_products.pop(0)
         prevline = line
     print_line(prevline)
 
