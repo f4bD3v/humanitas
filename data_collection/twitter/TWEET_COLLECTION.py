ea3686d918e2096114fd3dbbb4032b50f656f064
Joseph Boyd <josephboyd@lolumadbro.lan>
2014-04-21 23:47:14 +0200

    Tweet collection

--- data_collection/twitter/TWEET_COLLECTION.py
+++ data_collection/twitter/TWEET_COLLECTION.py
@@ -9,12 +9,8 @@ import pickle
 
 APP_KEY = 'aylam3pBvHkhOUmycOWw'
 APP_SECRET = 'uXAuZwGX8FUno0P54gdIlGnkijhkY56lVFxRwgjgI'
-
-twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
-ACCESS_TOKEN = twitter.obtain_access_token()
-twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-
 root = "KareenaOnline"
+
 RATE_LIMIT_WINDOW = 15 * 60
 TWEET_BATCH_SIZE = 200
 FOLLOWER_RATE_LIMIT = 30
@@ -59,7 +55,7 @@ def get_good_followers(followers, locations, min_tweets):
                     follower['screen_name'])
             protected += 1
             continue
-        for location_part in re.split('[ ,]+', re.sub(r'[^a-zA-Z, ]', '', 
+        for location_part in re.split('[ ,]+', re.sub(r'[^a-zA-Z, ]', '',
                 follower['location']).strip()):
             if location_part.lower() in locations:
                 good_location = True
@@ -88,14 +84,11 @@ def check_clock(last_time):
         print 'Sleeping for %s seconds...' % (RATE_LIMIT_WINDOW - elapsed_time)
         sleep(RATE_LIMIT_WINDOW - elapsed_time)
 
-def get_followers(all_followers):
+def get_followers(twitter):
     locations = set(get_all_locations_in_india())
     
-    if all_followers:
-        num_followers = min(MAX_FOLLOWERS, twitter.show_user(screen_name=root)['followers_count'])
-    else:
-        num_followers = 200
-
+    num_followers = min(MAX_FOLLOWERS, twitter.show_user(screen_name=root)['followers_count'])
+    
     next_cursor = -1; num_requests = 0; users_downloaded = 0; page_number = 1
 
     last_time = datetime.now()
@@ -115,19 +108,19 @@ def get_followers(all_followers):
         if num_requests == FOLLOWER_RATE_LIMIT:
             check_clock(last_time)
             last_time = datetime.now()
-            num_requests = 1                          
+            num_requests = 1
         next_cursor = response['next_cursor']
         users_downloaded += FOLLOWER_BATCH_SIZE
         page_number += 1
 
     f_followers = open('%s_good_followers_of.pickle'%(root), 'wb')
-    pickle.dump(good_followers, f_followers) 
+    pickle.dump(good_followers, f_followers)
     f_followers.close()
 
     duration = (datetime.now() - time_start).seconds
     print 'Number of good followers listed %s in %s seconds' % (len(good_followers), duration)
 
-def get_twitter_data():
+def get_twitter_data(twitter):
     f_followers = open('%s_good_followers_of.pickle'%(root), 'rb')
     followers = pickle.load(f_followers)
     f_followers.close()
@@ -161,7 +154,7 @@ def get_twitter_data():
                 check_clock(last_time)
                 last_time = datetime.now()
                 num_requests = 1
-            for tweet in new_tweets:                   
+            for tweet in new_tweets:
                 if max_tweet_id == 0 or max_tweet_id > int(tweet['id']):
                     max_tweet_id = int(tweet['id'])
             tweets.extend(new_tweets)
@@ -170,15 +163,22 @@ def get_twitter_data():
     pickle.dump(tweets, f_tweets)
     f_tweets.close()
 
-    duration = (datetime.now() - time_start).seconds    
+    duration = (datetime.now() - time_start).seconds
     print 'Number of tweets collected %s in %s seconds' % (len(tweets), duration)
     
 def main():
     global root
     if sys.argv > 1:
         root = sys.argv[1]
-    get_followers(False)
-    get_twitter_data()
+        APP_KEY = sys.argv[2]
+        APP_SECRET = sys.argv[3]
+
+    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+    ACCESS_TOKEN = twitter.obtain_access_token()
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    
+    get_followers(twitter)
+    get_twitter_data(twitter)
     
 if __name__ == '__main__':
     main()
