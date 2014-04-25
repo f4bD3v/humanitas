9c90b394db3109fb9b6a21687e6842e54056c7f9
fabbrix <brix.fabian@gmail.com>
2014-04-25 15:08:23 +0200

    cassandra keyspace+column family+indexes, esn update

---
+++ data_collection/social_media/twitter/azure/cassandra/db_definition.cql.sql
@@ -0,0 +1,33 @@
+
+# keyspaces are used to group column families together - similar to schema in RDBS
+CREATE KEYSPACE tweet_collector WITH
+strategy_class = 'SimpleStrategy'
+AND strategy_options:replication_factor=1;
+# replication_factor = 1 - no replication
+# different replication requirements should reside in different keyspaces
+
+use tweet_collector;
+
+# In Cassandra, you define column families. 
+# Column families can (and should) define metadata about the columns, 
+# but the actual columns that make up a row are determined by the client application. 
+# Each row can have a different set of columns. 
+# Each column family should be designed to contain a single type of data.
+
+# static column family for storing tweets
+CREATE TABLE tweets (
+    time timestamp,
+	user_id int,
+	region text,
+	city text,
+    content text,
+    lat float,
+    long float,
+   	rt boolean,
+    PRIMARY KEY (time));
+
+CREATE INDEX tweets_region
+  ON tweets (region);
+
+CREATE INDEX tweets_city
+  ON tweets (city);
--- models/esnn/esn.py
+++ models/esnn/esn.py
@@ -16,62 +16,86 @@ class ESN:
 		training tips:
 		- keep reservoir size small for selection of hyperparams
 	"""	
-	def __init__(self, N, R, P, input_size):
+	def __init__(self, data, N, R, P, input_size):
 
+		self._data = data
 
-		"""
-			random generation
-		"""
-
-		T = 30
-		Nu = T*R*P
-		N = 1825 # 5 years worth of daily data?
-
+		self._Nu = R*P
+		self._N = 1825 # 5 years worth of daily data?
+		self._initN = 365 # 1 year worth of initialization
 
 		"""
 			Reservoir size
 		"""
 		# "the bigger the size of the space of reservoir signals x(n), the easier it is to find a linear combination of the signals to approximate y_target(n)"
 		# N > 1+Nu+Nx should hold true
-		Nx = 1000 
+		self._Nx = 1000 
 
-		#input weights matrix 
-		"""
-			Input Scaling
-		"""
-		Win =  0
 
+		"""
+			###
+			RANDOM GENERATION 
+			###
+		"""
+
+		"""tan
+			+Input weight scaling
+		"""
+		self._Win = (random.rand(self._Nx,1+self._Nu)-0.5) * 1
 
 
 		"""
 			Sparsity of the reservoir
 		"""
-
 		# recurrent connection matrix
 		# speedup computation by making the reservoir matrix sparse - few connections between neurons in reservoir
 		# "fixed fanout number regardless of network size"
 		# uniform distribution of non-zero elements
-		W = random.rand(Nx,Nx)-0.5 
-
+		self._W = random.rand(self._Nx,self._Nx)-0.5 
 
 		"""
 			Spectral radius
 		"""
-
-		eigValuesVectors = linalg.eig(W)
+		eigValuesVectors = linalg.eig(self._W)
 		eigvalues = eigValuesVectors[0]
 
 		# spectral radius - max abs eigenvalue
 		rhoW = max(abs(eigValues))
 
 		# normalize random weight matrix with spectral radius
-		W/=rhoW
+		self._W/=rhoW
 
 		# for nonzero inputs u(n) rhoW < 1 is not a necessary condition for the echo state property 
 		# the spectral radius determines how fast the influence of an input dies out in a reservoir with time
 		# and how stable reservoir activations are
 		# => the spectral radius should be greater in tasks requiring longer memory of the input
 
+		"""
+			Design matrix [1,U,X]
+			keep N-initN time-steps in the design matrix, discard the initial initN steps
+		"""
+		self._bUX = zeros((1+Nu+Nx,N-initN))
+
+		"""
+			Target matrix 
+			same as input sequence, only shifted by 1 timestep 
+		"""
+		Yt = data[initN+1:N+1] 
 
 		# leaking rate - selected as free parameter
-		alpha =
+		self._alpha =
+
+	def train(self):		
+		# initial run vs. training
+		for t in range(N):
+			u = data[t]
+			xlast = self._x
+			xnext = tanh(dot(Win, vstack((1,u))+dot(W,xlast)
+			x = (1-self._alpha)*xlast+self._alpha*xnext
+
+	def get_error(self):
+
+
+
+
+
