c726e72df658349d988fe5de7e8336f094b5bab9
Gabriel Grill <gab.grill@gmail.com>
2014-04-26 01:30:32 +0200

    reshuffel of commands

--- data_collection/social_media/twitter/azure/cassandra/install-cassandra.sh
+++ data_collection/social_media/twitter/azure/cassandra/install-cassandra.sh
@@ -10,4 +10,5 @@ sudo mkdir -p /var/log/cassandra
 sudo chown -R `whoami` /var/log/cassandra  
 sudo mkdir -p /var/lib/cassandra  
 sudo chown -R `whoami` /var/lib/cassandra
+sudo pip install cassandra-driver
 
--- data_collection/social_media/twitter/azure/cassandra/install-deps.sh
+++ data_collection/social_media/twitter/azure/cassandra/install-deps.sh
@@ -1,13 +1,4 @@
 sudo apt-get install python-software-properties  
 sudo add-apt-repository ppa:webupd8team/java  
 sudo apt-get update  
-sudo apt-get install oracle-jdk7-installer  
-
-sudo apt-get install git
-
-sudo apt-get install ant1.7 ant1.7-optional  
-
-sudo apt-get install oracle-java7-set-default
-
-sudo apt-get install python-pip
-pip install cassandra-driver
+sudo apt-get install oracle-jdk7-installer git ant1.7 ant1.7-optional oracle-java7-set-default python-pip
