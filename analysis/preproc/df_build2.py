fbc52171e3f16b929105937a2f634cd72f712c9e
chingchia <fitsky.bw@gmail.com>
2014-04-26 17:24:46 +0200

    df_build2  reindex version. see usage

--- analysis/preproc/df_build2.py
+++ analysis/preproc/df_build2.py
@@ -6,16 +6,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-from collections import defaultdict
+
+usage = '''
+        df_full: a patched df with all missing dates filled with NaN.
+        df_ts: flattened version of df_full. Using parameters like
+               "with_interpolation" and "na_cutoff_rate".
+
+        If "using_df_ts" is False, get_full_data() will not generate df_ts.
+
+'''
 
 fp_csv = os.getcwd()+'/../../data/india/csv_weekly/rpms.dacnet.nic.in/all_commodities_weekly_india_'
 fp_state = os.getcwd()+'/../../data/india/csv_daily/agmarknet.nic.in/regions.csv'
 
-pk_out = 'india_week_df_full.pickle'
+pk_out1 = 'india_week_df_full.pickle'
+pk_out2 = 'india_week_df_ts.pickle'
 csv_out1 = 'india_week_full.csv'
 csv_out2 = 'india_week_timeseries.csv'
 date_freq = 'W-FRI'
-
+with_interpolation = False
+na_cutoff_rate = 0.3
+using_df_ts = True
 
 def get_raw():
     fp_lst = []
@@ -23,38 +34,32 @@ def get_raw():
         fp_lst.append("{}{}{}".format(fp_csv,i,".csv"))
     return csv2df_bulk(fp_lst)
 
+def get_state():
+    return pd.read_csv(fp_state)
+
 def get_data():
     start_time = time()
     df_raw = get_raw()
     df_reg = get_state()
     df = pd.merge(df_raw, df_reg, how="left", on="city")
+    print 'formatting dates'
+    df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
+    print 'sorting by dates'
+    df = df.sort('date')
     print("Cities not joined with states: "+str(list(set(df_raw["city"]) - set(df_reg["city"]))))
     print "{} {} {}".format("csv loaded into df in",(time()-start_time),'secs.')
     print "{} {}".format("size of df =", df.shape)
     return df
 
-def get_state():
-    return pd.read_csv(fp_state)
-
 def get_all_dates(df):
     all_dates_raw = sorted(list(set(df['date'])))
-    return pd.date_range(all_dates_raw[0], all_dates_raw[-1],
-            freq=date_freq)
+    return pd.date_range(all_dates_raw[0], all_dates_raw[-1],freq=date_freq)
 
 def mod_header(cols):
     cols = list(cols)
     cols[cols=='index'] = 'date'
     return cols
 
-def flatten(df, one = False):
-    df_ts = pd.DataFrame()
-    for (state, city, product, subproduct), group in \
-            df.groupby(['state', 'city','product','subproduct']):
-        group.set_index('date')
-        df_ts[(state, city, product, subproduct)] = group['price']
-        if one:
-            break
-    return df_ts
 
 def examine_fullness(df, leng):
     for (state, city, product, subproduct), group in \
@@ -64,35 +69,69 @@ def examine_fullness(df, leng):
         # elif group.shape[0] > leng:
         #     print 'over full', group.shape, (state, city, product, subproduct)
 
-def get_piece(missing_dates, old_row):
-    pieces = []
-    for date in missing_dates:
-        data =  [date]+list(old_row[1:6])+[float('nan'),old_row[-1]]
-        new_row = pd.DataFrame(data, index = list(old_row.index)).T
-        #print new_row
-        pieces.append(new_row)
+def replace_nan_with(x, t):
+    return x if isinstance(x, str) or isinstance(x, int) else t
 
-    return pd.concat(pieces)
+#
+# def get_piece(missing_dates, old_row):
+#     pieces = []
+#     for date in missing_dates:
+#         data =  [date]+list(old_row[1:6])+[float('nan'),old_row[-1]]
+#         new_row = pd.DataFrame(data, index = list(old_row.index)).T
+#         #print new_row
+#         pieces.append(new_row)
+#
+#     return pd.concat(pieces)
 
-def get_full_data(df, all_dates, examine=True, verbose=False):
-    #fill NaN into missing dates
-    #by generating missing date data
-    #and combining them with df
-
-    pieces = []
+def get_full_data(df, all_dates):
     start_time = time()
+    pieces = []
     count = 0
+    df_ts = pd.DataFrame()
 
-    for (state, city, product, subproduct), group in \
-            df.groupby(['state', 'city','product','subproduct']):
+    print 'Using df_ts?', using_df_ts
+
+    for (state, city, product, subproduct, country, freq), group in \
+            df.groupby(['state', 'city','product','subproduct', 'country', 'freq']):
+
+        # missing_dates = list(set(all_dates) - set(group['date']))
+        # # print missing_dates
+        # # print len(all_dates), len(group['date']), len(missing_dates)
+        # # break
+        # missing_piece = get_piece(missing_dates, group.iloc[0,])
+
+        #common part
+        group.set_index('date', inplace=True)
+
+        try:
+            group = group.reindex(all_dates)
+        except:
+            print (state, city, product, subproduct), 'dup dates'
+            continue
+
+        #df_ts part
+        # note that it's "larger than" valid_rate
+        if using_df_ts and group['price'].count() * 1. / len(all_dates) > 1. - na_cutoff_rate:
+            if with_interpolation:
+                df_ts[(product, subproduct, city, state)] = \
+                    group['price'].interpolate().bfill()
+            else:
+                df_ts[(product, subproduct, city, state)] = group['price']
+
+        #df_full part
+        group.reset_index(inplace=True)
+        group.columns = mod_header(group.columns)
+
+        group['freq'] = group['freq'].apply(lambda x: replace_nan_with(x, freq))
+        group['country'] = group['country'].apply(lambda x: replace_nan_with(x, country))
+        group['city'] = group['city'].apply(lambda x: replace_nan_with(x, city))
+        group['product'] = group['product'].apply(lambda x: replace_nan_with(x, product))
+        group['subproduct'] = group['subproduct'].apply(lambda x: replace_nan_with(x, subproduct))
+        group['state'] = group['state'].apply(lambda x: replace_nan_with(x, state))
+
+        pieces.append(group)
 
         count += 1
-        if verbose or (count % 1000 == 1):
-            print count, (state, city, product, subproduct)
-
-        missing_dates = list(set(all_dates) - set(group['date']))
-        missing_piece = get_piece(missing_dates, group.iloc[0,])
-        pieces.append(missing_piece)
 
     print 'number of labels = ', count
     print 'find all missing pieces in ', (time()-start_time)/60.0, ' min.'
@@ -100,37 +139,27 @@ def get_full_data(df, all_dates, examine=True, verbose=False):
     print 'combining'
     df_full = pd.concat([df]+pieces)
     print 'sorting by dates'
-    df_full['date'] = pd.to_datetime(df_full['date'])
     df_full = df_full.sort('date')
     print 'size of df_full = ', df_full.shape
 
-    #verify df_full contains all_dates
-    if examine:
-        examine_fullness(df_full, len(all_dates))
-    return df_full
+    return df_full, df_ts
 
 
 def main():
     df = get_data()
     all_dates = get_all_dates(df)
-    df_full = get_full_data(df, all_dates)
+    df_full, df_ts = get_full_data(df, all_dates)
+    #examine_fullness(df_full, len(all_dates))
 
-    with open(pk_out, 'wb') as f:
+    with open(pk_out1, 'wb') as f:
         pickle.dump(df_full, f)
 
     df_full.to_csv(csv_out1)
-    flatten(df_full, one=True).to_csv(csv_out2)
+
+    if using_df_ts:
+        with open(pk_out2, 'wb') as f:
+            pickle.dump(df_ts, f)
+        df_ts.to_csv(csv_out2)
 
 if __name__ == '__main__':
     main()
-    # df = get_data()
-    # all_dates = get_all_dates(df)
-    # #df_full = get_full_data(df, all_dates)
-    # pieces = []
-    # for (state, city, product, subproduct), group in \
-    #         df.groupby(['state', 'city','product','subproduct']):
-    #
-    #     missing_dates = list(set(all_dates) - set(group['date']))
-    #     missing_piece = get_piece(missing_dates, group.iloc[0,])
-    #     pieces.append(missing_piece)
-    #     break
