81a89f7dc87192f2c17262113d07fffa12898739
chingchia <fitsky.bw@gmail.com>
2014-04-27 14:26:54 +0200

    integrate daily and weekly data preprocessing

--- analysis/pca/pca.py
+++ analysis/pca/pca.py
@@ -3,29 +3,39 @@ from sklearn_pandas import DataFrameMapper, cross_val_score
 import pandas as pd
 import numpy as np
 import cPickle as pickle
+import sys
+sys.path.insert(0, '../preproc')
+from df_build2 import get_all_dates
 
-pk_in = 'india_week_df_full.pickle'
+pk_in = '../preproc/india_df_full_weekly.pickle'
+
+na_cutoff_rate = 0.3
+
 
 if __name__ == '__main__':
-    with open('pk_in','rb') as f:
+    with open(pk_in,'rb') as f:
         df = pickle.load(f)
     print 'df loaded in'
 
-    pbank = dict()
-    mbank = dict()
+    pcabank = dict()
+    mapbank = dict()
+    all_dates = get_all_dates(df)
 
-    #TODO
-    #untested!!!
     for state, sgroup in df.groupby('state'):
         for product, pgroup in sgroup.groupby('product'):
-            columns = []
-            for (subproduct, city), group in pgroup.groupby('subproduct','city'):
-                columns.append(group)
-                
-            mat = pd.concat(columns)
+            cols = []
+            for (subproduct, city), group in pgroup.groupby(['subproduct','city']):
+                group.set_index('date',inplace=True)
+                if group['price'].count() * 1. / len(all_dates) < 1 - na_cutoff_rate:
+                    continue
+                #group = group.interpolate().bfill()
+                cols.append(group['price'])
+
+            mat = pd.concat(cols)
             pca = PCA(1)
-            mapper = DataFrameMapper([(list(M.columns), pca)])
+            mapper = DataFrameMapper([(list(mat.columns), pca)])
             axis = mapper.fit_transform(mat)
-            pbank[(state, city, p2roduct, subproduct)] = pca
-            mbank[(state, city, product, subproduct)] = mapper
-            #mat_star = pca.inverse_transform(axis)
+            mat_star = pca.inverse_transform(axis)
+            pcabank[(state, city, product, subproduct)] = pca
+            mapbank[(state, city, product, subproduct)] = mapper
+            break
---
+++ analysis/preproc/df_run.py
@@ -0,0 +1,124 @@
+from df_build_func import *
+
+
+usage = '''
+    This script read India daily and/or weekly csv files into Pandas dataframes, df_full and/or df_ts
+
+    output:
+        india_df_full_weekly.pickle    india_df_full_daily.pickle
+        india_df_ts_weekly.pickle      india_df_ts_daily.pickle
+        india_full_weekly.csv          india_full_daily.csv
+        india_timeseries_weekly.csv    india_timeseries_daily.csv
+
+    description:
+        df      : an aggregate dataframe containing all csv data
+        df_full : a patched version of df with all missing dates filled with NaN price
+        df_ts   : a flattened version of df_full. Each column is a time series of
+                  prices of tuple (product, subproduct, city, state)
+
+    general options:
+        run_weekly         : True then convert india weekly into df_full and/or df_ts
+        run_daily          : True then convert india daily into df_full and/or df_ts
+        run_saving         : True then output all df_full and df_ts to pickles and csv's
+        using_df_full      : True then compute df_full
+        using_df_ts        : True then compute df_ts
+        daily_product_lst  : A list of products for importing daily dataset
+        filter_lst         : A list of products for pre-filtering on dataframes.
+                             Empty list means no pre-filtering. Once we decide
+                             which products to use, we set filter_lst = daily_product_lst,
+                             to avoid creating very large df_full and df_ts
+    df_ts options:
+        with_interpolation : True then do linear interpolation
+        na_cutoff_rate     : filter out those series with more NaN than this rate
+'''
+
+##============options================
+
+run_weekly = True
+run_daily = True
+run_saving = True
+
+using_df_full = True
+using_df_ts = True
+
+daily_product_lst = ['Rice']
+filter_lst = []
+
+with_interpolation = False
+na_cutoff_rate = 0.3
+
+##====================================
+
+fp_csv_weekly = os.getcwd()+'/../../data/india/csv_weekly/rpms.dacnet.nic.in/all_commodities_weekly_india_'
+fp_csv_daily = os.getcwd()+'/../../data/india/csv_daily/agmarknet.nic.in/daily/india_'
+fp_state = os.getcwd()+'/../../data/india/csv_daily/agmarknet.nic.in/regions.csv'
+
+pk_out1_template = 'india_df_full.pickle'   ## => "india_df_full_daily.pickle"
+pk_out2_template = 'india_df_ts.pickle'     ## => "india_df_ts_daily.pickle"
+
+csv_out1_template = 'india_full.csv'        ## => "india_full_daily.csv"
+csv_out2_template = 'india_timeseries.csv'  ## => "india_timeseries_daily.csv"
+
+date_freq_weekly = 'W-FRI'
+date_freq_daily = 'D'
+
+
+
+def main():
+    global run_weekly
+
+    for i in range(0,run_weekly+run_daily):
+
+        if run_weekly:
+            print '\n\nRunning weekly ===============================\n\n'
+            csv_out1_weekly = csv_out1_template.split('.')[0] + '_weekly.' + csv_out1_template.split('.')[1]
+            csv_out2_weekly = csv_out2_template.split('.')[0] + '_weekly.' + csv_out2_template.split('.')[1]
+            pk_out1_weekly = pk_out1_template.split('.')[0] + '_weekly.' + pk_out1_template.split('.')[1]
+            pk_out2_weekly = pk_out2_template.split('.')[0] + '_weekly.' + pk_out2_template.split('.')[1]
+
+            [fp_csv, date_freq, csv_out1, csv_out2, pk_out1, pk_out2] = \
+            [fp_csv_weekly, date_freq_weekly, csv_out1_weekly, csv_out2_weekly, pk_out1_weekly, pk_out2_weekly]
+
+            run_weekly = False
+
+        elif run_daily:
+            print '\n\nRunning daily ===============================\n\n'
+            csv_out1_daily = csv_out1_template.split('.')[0] + '_daily.' + csv_out1_template.split('.')[1]
+            csv_out2_daily = csv_out2_template.split('.')[0] + '_daily.' + csv_out2_template.split('.')[1]
+            pk_out1_daily = pk_out1_template.split('.')[0] + '_daily.' + pk_out1_template.split('.')[1]
+            pk_out2_daily = pk_out2_template.split('.')[0] + '_daily.' + pk_out2_template.split('.')[1]
+
+            [fp_csv, date_freq, csv_out1, csv_out2, pk_out1, pk_out2] = \
+            [fp_csv_daily, date_freq_daily, csv_out1_daily, csv_out2_daily, pk_out1_daily, pk_out2_daily]
+
+
+        df = get_data(fp_csv, fp_state, daily_product_lst)
+
+        all_dates = get_all_dates(df, date_freq)
+
+        df_full, df_ts = get_full_data(df, all_dates, \
+                using_df_full, using_df_ts, na_cutoff_rate, with_interpolation,\
+                filter_lst)
+
+        #examine_fullness(df_full, len(all_dates))
+
+        if run_saving:
+
+            if using_df_full:
+
+                print 'saving df'
+                with open(pk_out1, 'wb') as f:
+                    pickle.dump(df_full, f)
+                df_full.to_csv(csv_out1)
+
+            if using_df_ts:
+
+                print 'saving df_ts'
+                with open(pk_out2, 'wb') as f:
+                    pickle.dump(df_ts, f)
+                df_ts.to_csv(csv_out2)
+
+
+
+if __name__ == '__main__':
+    main()
