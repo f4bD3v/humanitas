c653f661489079bd7a4f6d16a1ae43f547d75aad
Joseph Boyd <josephboyd@LOLUMADBRO.local>
2014-05-02 21:51:32 +0200

    get food categories

---
+++ data_collection/social_media/twitter/azure/food_categories.py
@@ -0,0 +1,31 @@
+def get_food_words():
+    food_words = {}
+    food_words['rice'] = list(['rice'])
+    food_words['wheat'] = list(['wheat'])
+    food_words['banana'] = list(['banana'])
+    food_words['apple'] = list(['apple'])
+    food_words['coriander'] = list(['coriander'])
+    food_words['potato'] = list(['potato'])
+    food_words['onion'] = list(['onion'])
+    food_words['oil'] = list(['oil'])
+    food_words['bread'] = list(['bread'])
+    food_words['sugar'] = list(['sugar'])
+    food_words['salt'] = list(['salt'])
+    food_words['tea'] = list(['tea'])
+    food_words['coffee'] = list(['coffee'])
+    food_words['fish'] = list(['fish', 'carp', 'catfish', 'prawn'])
+    food_words['chicken'] = list(['chicken', 'poultry'])
+    food_words['corn'] = list(['corn', 'maize'])
+    food_words['nut'] = list(['nut'])
+    food_words['egg'] = list(['egg'])
+    food_words['milk'] = list(['milk'])
+    food_words['general'] = list(['breakfast', 'lunch', 'dinner', 'meal', 'food', 'eat'])
+    return food_words
+
+def print_foods():
+    foods = get_food_words()
+    for food in foods:
+        print foods[food]
+
+
+
