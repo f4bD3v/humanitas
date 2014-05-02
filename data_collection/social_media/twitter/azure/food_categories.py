579d1f359c038997a113be52ceea5c6617272141
Gabriel Grill <gab.grill@gmail.com>
2014-05-02 23:39:31 +0200

    small update of food categories

--- data_collection/social_media/twitter/azure/food_categories.py
+++ data_collection/social_media/twitter/azure/food_categories.py
@@ -1,14 +1,11 @@
 def get_food_words():
     food_words = {}
     food_words['rice'] = list(['rice'])
-    food_words['wheat'] = list(['wheat'])
-    food_words['banana'] = list(['banana'])
-    food_words['apple'] = list(['apple'])
+    food_words['wheat'] = list(['wheat', 'bread', 'flour'])
     food_words['coriander'] = list(['coriander'])
     food_words['potato'] = list(['potato'])
     food_words['onion'] = list(['onion'])
     food_words['oil'] = list(['oil'])
-    food_words['bread'] = list(['bread'])
     food_words['sugar'] = list(['sugar'])
     food_words['salt'] = list(['salt'])
     food_words['tea'] = list(['tea'])
@@ -16,7 +13,6 @@ def get_food_words():
     food_words['fish'] = list(['fish', 'carp', 'catfish', 'prawn'])
     food_words['chicken'] = list(['chicken', 'poultry'])
     food_words['corn'] = list(['corn', 'maize'])
-    food_words['nut'] = list(['nut'])
     food_words['egg'] = list(['egg'])
     food_words['milk'] = list(['milk'])
     food_words['general'] = list(['breakfast', 'lunch', 'dinner', 'meal', 'food', 'eat'])
