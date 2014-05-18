0a6f76790e419062f7266d326d29e5d1dfebb052
chingchia <fitsky.bw@gmail.com>
2014-05-19 00:37:34 +0200

    add script for merging series, update preprocess

--- analysis/preproc/df_run.py
+++ analysis/preproc/df_run.py
@@ -5,15 +5,15 @@ from df_build_func import *
 usage = '''
     This script read India daily and/or weekly csv files into Pandas dataframes, df_full and/or df_ts
 
-    sample output files (with na_cutoff_rate == 0.4):
-        india_original_weekly_0.4.csv
-        india_timeseries_weekly_0.4.csv
+    sample output files (with valid_rate == 0.6):
+        india_original_weekly_0.6.csv
+        india_timeseries_weekly_0.6.csv
 
-        india_original_wholesale_daily_0.4.csv
-        india_timeseries_wholesale_daily_0.4.csv
+        india_original_wholesale_daily_0.6.csv
+        india_timeseries_wholesale_daily_0.6.csv
 
-        india_original_retail_daily_0.4.csv
-        india_timeseries_retail_daily_0.4.csv
+        india_original_retail_daily_0.6.csv
+        india_timeseries_retail_daily_0.6.csv
 
     description:
         df      : an aggregate dataframe containing all csv data
@@ -34,15 +34,15 @@ usage = '''
         with_interpolation    : True then do linear interpolation on processed series
         interpolation_method  : 'linear', 'spline', 'polynomial'...
         interpolation_order   : the order of interpolation method 'spline' and 'polynomial'
-        na_cutoff_rate        : filter out those series with more NaN than this rate
+        valid_rate            : filter out those series with less valie data rate than this rate
 
 '''
 
 ##============options================
 
-run_retail_weekly = True
+run_retail_weekly = False
 run_wholesale_daily = True
-run_retail_daily = True
+run_retail_daily = False
 
 saving_csv = True
 saving_pickle = False
@@ -55,10 +55,11 @@ daily_product_lst = ['Rice','Wheat','Banana', 'Apple','Coriander','Potato', 'Oni
 filter_lst = ['Rice','Wheat','Banana', 'Apple','Coriander','Potato', 'Onion']
 
 with_interpolation = True
-interpolation_method = 'linear'
+interpolation_method = 'linear' #'spline', 'polynomial'
 interpolation_order = 3
 
-na_cutoff_rate = 0.4
+valid_rate = 0.6
+na_len_limit_ratio = 0.05
 
 ##====================================
 
@@ -70,8 +71,8 @@ fp_state = os.getcwd()+'/../../data/india/csv_daily/regions.csv'
 pk_out1_template = 'india_df_full.pickle'   ## => "india_df_full_daily_0,4.pickle"
 pk_out2_template = 'india_df_ts.pickle'     ## => "india_df_ts_daily_0.4.pickle"
 
-csv_out1_template = 'india_original.csv'        ## => "india_full_daily_0.4.csv"
-csv_out2_template = 'india_timeseries.csv'  ## => "india_timeseries_daily_0.4.csv"
+csv_out1_template00 = 'india_original.csv'        ## => "india_full_daily_0.4.csv"
+csv_out2_template00 = 'india_timeseries.csv'  ## => "india_timeseries_daily_0.4.csv"
 
 date_freq_weekly = 'W-FRI'
 date_freq_daily = 'D'
@@ -84,44 +85,62 @@ if __name__ == '__main__':
 
     for i in range(0,run_retail_weekly+run_wholesale_daily+run_retail_daily):
 
-        if run_retail_weekly:
-            print '\n\nRunning weekly ===============================\n\n'
+
+        if run_wholesale_daily:
+            print '\n\nRunning wholesale daily ===============================\n\n'
+            mid = '_wholesale_daily'
+            if with_interpolation:
+                mid = mid + '_interpolated'
+            fp = os.getcwd()+'/wholesale_daily/'
+            if not os.path.exists(fp):
+                os.makedirs(fp)
+            csv_out1_template = fp+csv_out1_template00
+            csv_out2_template = fp+csv_out2_template00
+
+            csv_out1_daily = csv_out1_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + csv_out1_template.split('.')[1]
+            csv_out2_daily = csv_out2_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + csv_out2_template.split('.')[1]
+            pk_out1_daily = pk_out1_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + pk_out1_template.split('.')[1]
+            pk_out2_daily = pk_out2_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + pk_out2_template.split('.')[1]
+
+            [fp_csv, date_freq, csv_out1, csv_out2, pk_out1, pk_out2] = \
+            [fp_csv_daily, date_freq_daily, csv_out1_daily, csv_out2_daily, pk_out1_daily, pk_out2_daily]
+
+        elif run_retail_weekly:
+            print '\n\nRunning retail weekly ===============================\n\n'
             mid = '_retail_weekly'
             if with_interpolation:
                 mid = mid + '_interpolated'
-            csv_out1_weekly = csv_out1_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + csv_out1_template.split('.')[1]
-            csv_out2_weekly = csv_out2_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + csv_out2_template.split('.')[1]
-            pk_out1_weekly = pk_out1_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + pk_out1_template.split('.')[1]
-            pk_out2_weekly = pk_out2_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + pk_out2_template.split('.')[1]
+            fp = os.getcwd()+'/retail_weekly/'
+            if not os.path.exists(fp):
+                os.makedirs(fp)
+            csv_out1_template = fp+csv_out1_template00
+            csv_out2_template = fp+csv_out2_template00
+
+            csv_out1_weekly = csv_out1_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + csv_out1_template.split('.')[1]
+            csv_out2_weekly = csv_out2_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + csv_out2_template.split('.')[1]
+            pk_out1_weekly = pk_out1_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + pk_out1_template.split('.')[1]
+            pk_out2_weekly = pk_out2_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + pk_out2_template.split('.')[1]
 
             [fp_csv, date_freq, csv_out1, csv_out2, pk_out1, pk_out2] = \
             [fp_csv_weekly, date_freq_weekly, csv_out1_weekly, csv_out2_weekly, pk_out1_weekly, pk_out2_weekly]
 
             run_retail_weekly = False
 
-        elif run_wholesale_daily:
-            print '\n\nRunning wholesale daily ===============================\n\n'
-            mid = '_wholesale_daily'
-            if with_interpolation:
-                mid = mid + '_interpolated'
-            csv_out1_daily = csv_out1_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + csv_out1_template.split('.')[1]
-            csv_out2_daily = csv_out2_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + csv_out2_template.split('.')[1]
-            pk_out1_daily = pk_out1_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + pk_out1_template.split('.')[1]
-            pk_out2_daily = pk_out2_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + pk_out2_template.split('.')[1]
-
-            [fp_csv, date_freq, csv_out1, csv_out2, pk_out1, pk_out2] = \
-            [fp_csv_daily, date_freq_daily, csv_out1_daily, csv_out2_daily, pk_out1_daily, pk_out2_daily]
-            run_wholesale_daily = False
-
         elif run_retail_daily:
             print '\n\nRunning retail daily ===============================\n\n'
             mid = '_retail_daily'
             if with_interpolation:
                 mid = mid + '_interpolated'
-            csv_out1_daily = csv_out1_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + csv_out1_template.split('.')[1]
-            csv_out2_daily = csv_out2_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + csv_out2_template.split('.')[1]
-            pk_out1_daily = pk_out1_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + pk_out1_template.split('.')[1]
-            pk_out2_daily = pk_out2_template.split('.')[0] + mid + '_'+str(na_cutoff_rate)+'.' + pk_out2_template.split('.')[1]
+            fp = os.getcwd()+'/retail_daily/'
+            if not os.path.exists(fp):
+                os.makedirs(fp)
+            csv_out1_template = fp+csv_out1_template00
+            csv_out2_template = fp+csv_out2_template00
+
+            csv_out1_daily = csv_out1_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + csv_out1_template.split('.')[1]
+            csv_out2_daily = csv_out2_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + csv_out2_template.split('.')[1]
+            pk_out1_daily = pk_out1_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + pk_out1_template.split('.')[1]
+            pk_out2_daily = pk_out2_template.split('.')[0] + mid + '_'+str(valid_rate)+'.' + pk_out2_template.split('.')[1]
 
             [fp_csv, date_freq, csv_out1, csv_out2, pk_out1, pk_out2] = \
             [fp_csv_daily_retail, date_freq_daily, csv_out1_daily, csv_out2_daily, pk_out1_daily, pk_out2_daily]
@@ -131,9 +150,14 @@ if __name__ == '__main__':
 
         all_dates = get_all_dates(df, date_freq)
 
-        df_full, df_ts, dup_records, na_table, probe = get_full_data(df, all_dates, \
-                using_df_full, using_df_ts, na_cutoff_rate, with_interpolation,\
-                interpolation_method, interpolation_order, filter_lst)
+        df_full, df_ts, dup_records, valid_table, probe = get_full_data(df, all_dates, \
+                using_df_full, using_df_ts, valid_rate, with_interpolation,\
+                interpolation_method, interpolation_order, filter_lst, na_len_limit_ratio)
+
+        if run_wholesale_daily:
+            df_full = df_full/100
+            df_ts = df_ts/100
+            run_wholesale_daily = False
 
         df_ts_ret = get_ret(df_ts)
         #df_ts = remove_spikes(df_ts, 100)
@@ -166,7 +190,7 @@ if __name__ == '__main__':
             if using_df_ts:
                 print 'saving df_ts to csv'
                 df_ts.to_csv(csv_out2, index_label='date')
-                df_ts_ret.to_csv(csv_out2+'ret', index_label='date')
+                df_ts_ret.to_csv(csv_out2[:-4]+'_ret'+csv_out2[-4:], index_label='date')
 
         if saving_na_analysis:
             print 'saving na_table to csv'
---
+++ analysis/preproc/merge_series.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+wholesale_daily = True
+retail_daily = False
+retail_weekly = False
+
+output_by_city = False
+merge_by_prod_reg = True
+plot_everything = False
+
+if wholesale_daily+retail_daily+retail_weekly != 1:
+    raise Exception('exactly one option being True required!')
+elif wholesale_daily:
+    csv_in = os.getcwd()+'/wholesale_daily/india_timeseries_wholesale_daily_interpolated_0.6.csv'
+    out_folder = os.getcwd()+'/wholesale_daily/'
+elif retail_daily:
+    csv_in = os.getcwd()+'/retail_daily/india_timeseries_retail_daily_interpolated_0.6.csv'
+    out_folder = os.getcwd()+'/retail_daily/'
+elif retail_weekly:
+    csv_in = os.getcwd()+'/retail_weekly/india_timeseries_retail_weekly_interpolated_0.6.csv'
+    out_folder = os.getcwd()+'/retail_weekly/'
+
+
+if not os.path.exists(out_folder):
+    os.makedirs(out_folder)
+
+
+# csv_out_wholesale_daily = 'wholesale_daily'
+
+def subcolnames(df_ts, q1, q2):
+    cnames = []
+    for str_label in list(df_ts.columns):
+        if (q1 in str_label) and (q2 == None or q2 in str_label):
+            cnames.append(str_label)
+    return cnames
+
+def subdf(df_ts, q1, q2 = None):
+    cnames = subcolnames(df_ts, q1, q2)
+    return df_ts[cnames]
+
+def clear_symbols(string_lst):
+    symbols = ['(',')',',','\'']
+    ret = []
+    for string in string_lst:
+        for s in symbols:
+            string = string.replace(s, '')
+        ret.append(string)
+    if len(ret) != 4:
+        raise Exception('incorrect label parsing: '+str(string_lst))
+    return ret
+
+def parse_strlabel_to_tuple(strlabel):
+    label = clear_symbols(strlabel.split(', '))
+    return label[0], label[1], label[2], label[3]
+
+def parse_colnames_to_tuples(df_ts):
+    cc = []
+    for strlabel in list(df_ts.columns):
+        cc.append(parse_strlabel_to_tuple(strlabel))
+    return cc
+
+
+def all_state_city_prod_subprod(df_ts):
+    all_states = set()
+    all_cities = set()
+    all_products = set()
+    all_subproducts = set()
+
+    for (state, city, product, subproduct) in list(df_ts.columns):
+        #  = parse_strlabel_to_tuple(strlabel)
+        all_states.add(state)
+        all_cities.add(city)
+        all_products.add(product)
+        all_subproducts.add(subproduct)
+
+    return sorted(list(all_states)), sorted(list(all_cities)), sorted(list(all_products)), sorted(list(all_subproducts))
+
+def run_output_by_city(df_ts, all_cities):
+    df_duy = pd.DataFrame()
+    for city in all_cities:
+        df_city = subdf(df_ts, city)
+        for (state, city, product, subproduct) in list(df_city.columns):
+            df_duy[product+'_'+subproduct] = df_city[(state, city, product, subproduct)]
+        fp = out_folder+'by_city/'
+        if not os.path.exists(fp):
+            os.makedirs(fp)
+        fp = fp+city+'.csv'
+        df_duy.to_csv(fp, index_label='date')
+
+def run_merge_by_prod_reg(df_ts, all_states, all_products):
+    df_merge = pd.DataFrame()
+
+    for state in all_states:
+        for product in all_products:
+            df_this = subdf(df_ts, state, product)
+
+            if df_this.shape[1] == 0:
+                continue
+
+            avg_series = df_this.mean(axis=1)
+            df_merge[(state, product)] = avg_series
+
+    return df_merge
+
+
+def plotter(df, fpath, fname, save=False, close=True, legend=True):
+    if not os.path.exists(fpath):
+        os.makedirs(fpath)
+    fp = fpath+fname
+
+    df.plot(legend=legend)
+
+    if save:
+        plt.savefig(fp)
+    if close:
+        plt.close()
+    return True
+
+def read_df_ts(csv_in):
+    df_ts = pd.read_csv(csv_in)
+    df_ts.set_index('date', inplace=True)
+    df_ts.columns = parse_colnames_to_tuples(df_ts)
+    return df_ts
+
+def clear_slash(string):
+    return string.replace('/', '')
+
+if __name__ == '__main__':
+    df_ts = read_df_ts(csv_in)
+    all_states, all_cities, all_products, all_subproducts = all_state_city_prod_subprod(df_ts)
+
+    if output_by_city:
+        run_output_by_city(df_ts, all_cities)
+
+    if merge_by_prod_reg:
+        df_merge = run_merge_by_prod_reg(df_ts, all_states, all_products)
+
+        for state in all_states:
+            df_state = subdf(df_merge, state)
+            # df_state.plot()
+            plotter(df_state, fpath = out_folder+'plot_merge/', fname=state+'.png', save=True)
+
+
+        #save to csv by region
+        for state in all_states:
+            df_reg = pd.DataFrame()
+            for product in all_products:
+                series = subdf(df_merge, state, product)
+                if series.shape[1] != 0:
+                    df_reg[product] = series
+
+            fp = out_folder+'merged_by_reg/'
+            if not os.path.exists(fp):
+                os.makedirs(fp)
+
+            fp = fp+state+'.csv'
+            df_reg.to_csv(fp, index_label='date')
+
+    if plot_everything:
+        for label in list(df_ts.columns):
+            plotter(df_ts[label], fpath = out_folder+'plot_everything/', fname=str(label).replace('/','-')+'.png', save=True)
